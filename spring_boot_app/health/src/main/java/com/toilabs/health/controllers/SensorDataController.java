a269adedd0aa7617171406630c7646f10a6973a3
Shazeb Ata <ataata107@gmail.com>
2025-07-20 21:40:42 -0700

    corrected health checks

--- spring_boot_app/health/src/main/java/com/toilabs/health/controllers/SensorDataController.java
+++ spring_boot_app/health/src/main/java/com/toilabs/health/controllers/SensorDataController.java
@@ -5,6 +5,8 @@ import com.toilabs.health.repository.SensorDataRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
 
 import java.util.List;
 
